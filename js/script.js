58e431c2dbca00070e85f0fe4a3ca2ef3385b674
AlexanderBil <deos.ninja@gmail.com>
2020-11-13 22:50:06 +0200

    new commit

--- js/script.js
+++ js/script.js
@@ -28,6 +28,8 @@ document.querySelector('.burger-wrapper').onclick = togle;
 function togle() {
     document.querySelector('.polosa').classList.toggle('polosa-active'); // анимация крестика
     document.querySelector('.mob-menu').classList.toggle('active'); // выдвижение меню
+    document.querySelector('.main-menu-icon').classList.toggle('active-1') 
+    document.querySelector('.mob-menu-content').classList.toggle('active-2')   //плавно выезжает мобильное меню
 }
 
 document.querySelector('.mob-menu').onclick = togle2; // для задвижения меню по клику на него и анимация крестика
@@ -36,6 +38,22 @@ function togle2() {
     let menu = document.querySelector('.mob-menu');
     if (menu.classList.contains('active')) {
         menu.classList.remove('active')
+
     }
     document.querySelector('.polosa').classList.toggle('polosa-active');
+    document.querySelector('.mob-menu-content').classList.toggle('active-2') //плавно выезжает мобильное меню
 }
+
+
+
+//=================Переключение стартовой картинки при < 700==========================
+
+window.onresize = function(event) {
+    if(event.currentTarget.innerWidth < 700){
+        document.querySelector('.myImage').src="img/block-1/5.jpg";
+    }else{
+        document.querySelector('.myImage').src="img/block-1/2.jpg";
+    }
+};
+
+
